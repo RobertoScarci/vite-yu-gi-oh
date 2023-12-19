c1f1c29f8b01b362b77f90dc15a1e7974ef0c71f
RobertoScarci <robertoscarci.02@gmail.com>
2023-12-19 15:18:48 +0100

    creazione della ricerca delle carte in base alla tipologia della carta in questione

---
+++ my-vue-app/src/components/AppSearch.vue
@@ -0,0 +1,37 @@
+<script>
+    export default {
+    name: "AppSearch",
+    data() {
+        return {
+            selectedArchetype: ""
+        }
+    },
+    props: {
+        archetypeList: {
+            type: Object,
+            required: true
+        }
+    },
+    }
+</script>
+
+<template>
+    <div class="container mb-3">
+        <div class="row">
+            <div class="col-2">
+                <select class="form-select" aria-label="Default select example" @click="$emit('search',selectedArchetype)" v-model="selectedArchetype">
+                    <option v-for="archetype in archetypeList" :value="archetype">{{ archetype }}</option>
+                </select>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+select{
+    margin-left: 4rem;
+    margin-top: 1rem;
+}
+
+</style>
