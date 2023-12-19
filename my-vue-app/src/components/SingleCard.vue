56bbcb3261b69ac6884c123c270bac317f5fb771
RobertoScarci <robertoscarci.02@gmail.com>
2023-12-19 15:18:24 +0100

    creazione della singola card dove verranno inserite le singole carte

---
+++ my-vue-app/src/components/SingleCard.vue
@@ -0,0 +1,28 @@
+<script>
+    export default {
+    name: "SingleCard",
+    props: {
+        card: {
+            type: Object,
+            required: true
+        }
+    }
+    }
+</script>
+
+<template>
+    <div class="card col-12 col-md-4 col-lg-2 mb-4 me-3">
+        <img :src="card.card_images[0].image_url" class="img-fluid " :alt=" card.name + '\'s card'">
+        <div class="card-body">
+            <h6 class="card-title text-white"> {{ card.name }} </h6>
+            <p class="card-text"> {{ card.archetype }} </p>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+div{
+    background-color: #D48F37;
+}
+</style>
