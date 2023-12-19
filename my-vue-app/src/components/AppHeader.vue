6ad66ec9032cb1eaca4be445cc02f6215db5b261
RobertoScarci <robertoscarci.02@gmail.com>
2023-12-19 15:17:12 +0100

    creazione e struttura dell'header

---
+++ my-vue-app/src/components/AppHeader.vue
@@ -0,0 +1,28 @@
+<script>
+    export default {
+        name: "AppHeader"
+    }
+</script>
+
+<template>
+    <header>
+        <nav class="mb-3">
+            <div class="container">
+                <img src="https://vignette1.wikia.nocookie.net/yugiohenespanol/images/a/aa/Yugioh_anime_logo.jpg/revision/latest?cb=20130310042737&path-prefix=es" alt="" class="mb-3">
+                <span class=" fs-1 m-3">Yu-Gi-Oh Api</span>
+            </div>
+        </nav>
+    </header>
+</template>
+
+<style lang="scss" scoped>
+
+img{
+    width: 4%;
+}
+
+header{
+    background-color: white;
+}
+
+</style>
